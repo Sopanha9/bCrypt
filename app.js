428e12fb629840794a63dbf3ad7e70110f8c3e5e
John <lolznha72@gmail.com>
2025-12-16 13:53:37 +0700

    bcrpyt with normal array

---
+++ app.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10); // mininum should at least 10
+  users.push({ username, password: hashedPassword });
+  res.status(201).send('User registered');
+  console.log(users);
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = users.find(u => u.username == username);
+  if (!user) {
+    res.status(404).send('Wrong username');
+  }
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    res.status(401).send('Wrong password');
+  }
+  res.status(200).send('Logged in');
+  
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
